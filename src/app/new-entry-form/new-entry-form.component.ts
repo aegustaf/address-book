cd1d60ebbad0e05b1642047c02b36bf527fce324
aegustaf <agustafson52@gmail.com>
2021-01-03 21:26:56 -0500

    Add new contact submission logic and go back to homepage on submit

--- src/app/new-entry-form/new-entry-form.component.ts
+++ src/app/new-entry-form/new-entry-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Location } from '@angular/common';
+import { Entry } from '../entries/entry';
+import { ENTRIES_LIST } from '../entries-list';
 
 @Component({
   selector: 'app-new-entry-form',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewEntryFormComponent implements OnInit {
 
-  constructor() { }
+  newEntryForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private location: Location) {
+    this.newEntryForm = this.formBuilder.group(
+      {
+        name: '',
+        address: '',
+        email: '',
+        phone: ''
+      }
+    );
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(entryData): void {
+    let entry: Entry = new Entry(entryData.name, entryData.address, entryData.email, entryData.phone);
+    ENTRIES_LIST.push(new Entry(entryData.name, entryData.address, entryData.email, entryData.phone));
+    this.location.back();
   }
 
 }
