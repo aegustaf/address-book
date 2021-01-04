bfdc01a441b2c8a78161f0807da3866f1a7393ab
aegustaf <agustafson52@gmail.com>
2021-01-04 12:51:42 -0500

    Add entries for testing

--- src/app/entries-list.ts
+++ src/app/entries-list.ts
@@ -1,3 +1,7 @@
 import { Entry } from './entries/entry';
 
-export let ENTRIES_LIST: Entry[] = [];
+export let ENTRIES_LIST: Entry[] = [
+    new Entry(0, 'Ariana Grande', '123 State St', 'ag@aol.com', '123-123-1234'),
+    new Entry(1, 'Heidi Klum', '92 MLK Blvd', 'hk@aol.com', '123-123-1234'),
+    new Entry(2, 'Miley Cyrus', '456 Nebraska Ave', 'mc@aol.com', '123-123-1234')
+  ];
