637ec22c1d31a491ef634b6cb16e6f04cc4058fc
aegustaf <agustafson52@gmail.com>
2021-01-03 20:32:55 -0500

    Add basic form for creating new contacts for address book

---
+++ src/app/new-entry-form/new-entry-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-entry-form',
+  templateUrl: './new-entry-form.component.html',
+  styleUrls: ['./new-entry-form.component.scss']
+})
+export class NewEntryFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
