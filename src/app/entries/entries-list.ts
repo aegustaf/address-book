a3b07f0ce627a00c2e914510c6e2689ce83dec7b
aegustaf <agustafson52@gmail.com>
2021-01-05 12:27:17 -0500

    fixed bug to sort contacts regardless of lowercase/uppercase

--- src/app/entries/entries-list.ts
+++ src/app/entries/entries-list.ts
@@ -17,7 +17,7 @@ export class EntriesList {
 
   static getEntriesOrderedAlphabetically(): Entry[] {
     return this.entriesList.slice().sort((a, b) => {
-      if (a.name < b.name) {
+      if (a.name.toLowerCase() < b.name.toLowerCase()) {
         return -1;
       } else if (a.name.toLowerCase() > b.name.toLowerCase()) {
         return 1;
